f696683f722e74e0fbb5ab8638050935b0c3d068
smiggleworth <jrepanich@gmail.com>
2025-08-07 07:31:45 -0400

    update constants

--- http_constants.go
+++ http_constants.go
@@ -1,5 +1,8 @@
 package mux
 
+// HTTP constants for MIME types and headers used in web APIs and HTTP communication.
+// This file provides commonly used constants to avoid magic strings and improve maintainability.
+
 // Common MIME types used in web APIs and HTTP
 const (
 	MimeJSON              = "application/json"
@@ -24,4 +27,13 @@ const (
 	MimeJSONAPI           = "application/vnd.api+json"
 	MimeOpenAPI           = "application/vnd.oai.openapi"
 	MimeYAML              = "application/x-yaml"
+	MimeProblemJSON       = "application/problem+json"
+)
+
+// Common HTTP header names
+const (
+	HeaderContentType        = "Content-Type"
+	HeaderContentDisposition = "Content-Disposition"
+	HeaderXForwardedFor      = "X-Forwarded-For"
+	HeaderXForwardedProto    = "X-Forwarded-Proto"
 )
