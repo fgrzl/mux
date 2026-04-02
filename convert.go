708623f627b27258fe78a7af32b3bee0f1509952
smiggleworth <jrepanich@gmail.com>
2026-04-02 09:40:38 -0400

    refactor: remove unused JSON conversion function from convert.go

--- convert.go
+++ convert.go
@@ -1,8 +1,6 @@
 package mux
 
 import (
-	"encoding/json"
-
 	internalopenapi "github.com/fgrzl/mux/internal/openapi"
 )
 
@@ -13,21 +11,6 @@ func cloneSlice[T any](in []T) []T {
 	out := make([]T, len(in))
 	copy(out, in)
 	return out
-}
-
-func convertViaJSON[T any](in any) (T, error) {
-	var out T
-	if in == nil {
-		return out, nil
-	}
-	data, err := json.Marshal(in)
-	if err != nil {
-		return out, err
-	}
-	if err := json.Unmarshal(data, &out); err != nil {
-		return out, err
-	}
-	return out, nil
 }
 
 func toInternalSecurityRequirement(sec SecurityRequirement) *internalopenapi.SecurityRequirement {
